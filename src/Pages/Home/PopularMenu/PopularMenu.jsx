b499df68f49acc9304c6ab6b91ce14d4439b4c76
aminul-islam01 <abiddut953@gmail.com>
2023-05-23 15:31:31 +0600

    add banner and category section

---
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,16 @@
+import SectionTitle from "../../../Components/SectionTitle";
+
+
+const PopularMenu = () => {
+    return (
+        <div>
+            <SectionTitle
+            subHeading="Check it out"
+            heading="From Our Menu">
+            </SectionTitle>
+            
+        </div>
+    );
+};
+
+export default PopularMenu;
