07aad2f340b5f893b8b75f626e16a1d5e25ae03e
aminul-islam01 <abiddut953@gmail.com>
2023-05-28 17:40:18 +0600

    add to cart data dynamic

--- src/Components/FoodCard.jsx
+++ src/Components/FoodCard.jsx
@@ -1,7 +1,37 @@
+import Swal from "sweetalert2";
+import useCart from "../UseMenu/UseCart";
+import { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProviders";
 
 
-const FoodCard = ({item}) => {
-    const {image, name, recipe, price} = item;
+const FoodCard = ({ item }) => {
+    const {user} = useContext(AuthContext);
+    const { image, name, recipe, price } = item;
+    const [, refetch] = useCart();
+
+    const handleAddToCart = (item) => {
+        const { _id, image, name, price } = item;
+        const cartItem = { foodId: _id, image, name, price, email: user.email };
+        fetch('http://localhost:5000/carts', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(cartItem)
+        })
+            .then(res => res.json())
+            .then(() => {
+                refetch()
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Food added on the cart.',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+                
+            })
+    }
+
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure><img src={image} /></figure>
@@ -10,7 +40,7 @@ const FoodCard = ({item}) => {
                 <h2 className=" text-3xl font-bold text-center">{name}</h2>
                 <p className="text-left">{recipe}</p>
                 <div className="card-actions justify-center">
-                    <button className="btn btn-primary">Add To Card</button>
+                    <button onClick={() => handleAddToCart(item)} className="btn btn-primary">Add To Card</button>
                 </div>
             </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,19 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './Routes/Routes'
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProviders from './Provider/AuthProviders'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProviders>
-      <HelmetProvider>
-        <RouterProvider router={router} />
-      </HelmetProvider>
-    </AuthProviders>
+    <QueryClientProvider client={queryClient}>
+      <AuthProviders>
+        <HelmetProvider>
+          <RouterProvider router={router} />
+        </HelmetProvider>
+      </AuthProviders>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
